5eedbdd16c012fcb363494d0389fc2de3fb0dc5c
Rafael Ojeda <RafaelOjeda@users.noreply.github.com>
2022-07-27 16:09:07 -0400

    All Operands & Operators Are Now Added To The Correct Stack

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,51 @@ public class Main{
         return s.nextInt();
     }
     public static void main(String args[]) throws Exception{
+
+        Stack<Character> operators = new Stack<>();
+        Stack<Integer> operands = new Stack<>();
+
         Scanner s = new Scanner(System.in);
-        while(true){
+        while(true) {
             System.out.println("Please enter a new expression: ");
             String exp = s.nextLine();
-            if(exp.equals("DONE"))
+
+            if(exp.equals("DONE")) {
                 break;
-            int result = 0;
-            boolean add = true;//if false, means subtract
-            for(int cur = 0; cur < exp.length();cur++){
-                if(exp.charAt(cur) == '+')
-                    add = true;
-                else if (exp.charAt(cur) == '-')
-                    add = false;
+            }
+//            int result = 0;
+//            boolean add = true;//if false, means subtract
+            for(int cur = 0; cur < exp.length();cur++) {
+                char current = exp.charAt(cur);
+
+                if(current == '+') { // Adds + to operators
+//                    add = true;
+                    operators.push(current);
+
+                } else if (current == '-') { // Adds - to operators
+//                    add = false;
+                    operators.push(current);
+                } else if (current == '(') {
+                    operators.push(current);
+                } else if (current == ')') {
+                    operators.push(current);
+                } else if (Character.isSpaceChar(current)) {
+                    continue;
+                }
                 else if (Character.isDigit(exp.charAt(cur))){
                     int operand = getOp(exp, cur);
-                    while(cur < exp.length() && Character.isDigit(exp.charAt(cur)))
+                    operands.push(operand); // Adds current operand onto the operands stack
+
+                    while(cur < exp.length() && Character.isDigit(exp.charAt(cur))) {
                         cur++;
+                    }
                     cur--;
-                    result = add? result + operand: result - operand;
-                }else
+                } else // Error handling
                     throw new Exception("Error: bad input");
             }
-            System.out.println("The result is " + result);
+//            System.out.println("The result is " + result);
+            System.out.println("Number Stack: " + operands);
+            System.out.println("Operator Stack" + operators);
         }
     }
 }
